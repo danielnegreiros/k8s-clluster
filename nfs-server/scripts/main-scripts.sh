28cc37bd8fb6a0a289f8833701a64e1bdd84f411
daniel barros <danielnegreirosb@gmail.com>
2022-04-24 12:02:27 -0300

    rm nfs-server

--- nfs-server/scripts/main-scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo -i 
-
-## Set Vagrant password and allow password ssh
-echo "test123" | passwd --stdin vagrant
-sed -i 's/#PasswordAuthentication/PasswordAuthentication/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
-
